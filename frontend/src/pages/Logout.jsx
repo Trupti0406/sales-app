d8aaa6d1bc98fbbe416db2e230d01c618c6d8555
trupti0406 <truptivijayyadav27@gmail.com>
2023-02-25 00:15:30 +0530

    frontend done

--- frontend/src/pages/Logout.jsx
+++ frontend/src/pages/Logout.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const Logout = () => {
   return (
     <div className="container">
-      <h1 className="text-center fw-bolder mb-5">Are you sure youwant to Logout?</h1>
+      <h1 className="text-center fw-bolder mb-5">
+        Are you sure youwant to Logout?
+      </h1>
       <div className="d-grid col-3 m-auto">
         <button className="btn py-2 fs-5 fw-bold" type="button">
           Logout
